54218adb19ceea266f173e215182f2f0521a22e5
Nick Brown <n.brown.professional@gmail.com>
2018-03-01 10:54:57 -0700

    Commit adds working product of app and major style changes
    - In this commit the search tool is functioning properly with one acception
    --when a user types in a word and clicks submit a button will be added to the button list
    --if a user clicks on a button, it will call the GIPHY API and return 10 gifs related to th
    e button
    --(issue) if user types in word to the input, then imediatly hits enter the page reloads
    
    -adds style changes
    --background for page, gifs and borders are changed to show a them of black and alternating
     colors of the rainbow
    --text and content is cetered to the page and is made moderately responsive (limited testing completed)
    ~
    ~

--- assets/logic.js
+++ assets/logic.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    
+
     // Global Variables
 
     var emotionGifs = ["Happy", "Sad", "Hungover", "Tired", "LET'S GO!", "Excited", "Stoked" , "Sick", "Winning", "Frustrated"];
@@ -12,26 +12,16 @@ $(document).ready(function () {
     // var newItem = $("#emotionInput").val().trim();
     var newItem;
 
-    // Working API URLs
-    // https://api.giphy.com/v1/gifs/search?api_key=iiHxX5ZyrJst3sHz4lwHPzpO14Eot1nx&q=happy&limit=10&offset=0&&lang=en 
-
-    // var searchTerm;
-    // var apiURLBase = "https://api.giphy.com/v1/gifs/search?api_key=iiHxX5ZyrJst3sHz4lwHPzpO14Eot1nx&q=random&limit=10&offset=0&&lang=en";
-
     // Loops through the emotionGifs array and creates buttons
    function buttonPrint() {
        
         $("#emotionButtons").empty();
    
-        console.log(emotionGifs);
-        
         for (var i =0; i < emotionGifs.length; i++) {
             var newEmotionButton = $("<button>" + emotionGifs[i] + "</button>");
             newEmotionButton.attr("id", "button_" + emotionGifs[i]);
             newEmotionButton.attr("data-value", emotionGifs[i]);
             $("#emotionButtons").append(newEmotionButton)
-            console.log(newEmotionButton.attr("data-value", emotionGifs[i]));
-            console.log(newEmotionButton);
         }
 
     }
@@ -42,38 +32,25 @@ $(document).ready(function () {
 
     // When submit button is pressed
     $("#addEmotion").on("click", function(){
+        // event.preventDefault();  <------- make sure if you add this back in to put "event" into the function parameters above
         newItem = $("#emotionInput").val().trim();
-        if (emotionGifs.includes(newItem)!== true) {
+        if (newItem.length === 0) {
+            alert("Type a work in the textbox to add a button.");
+        }
+        else if (emotionGifs.includes(newItem)!== true) {
            emotionGifs.push(newItem);
-            console.log(emotionGifs);
             buttonPrint(); 
         } else {
             alert("You already have a button for that!");
         }
         $("#emotionInput").val("");
 
-    });
-
-    // $("#removeEmotion").on("click", function () {
-        
-    //     if (emotionGifs.includes(newItem)) {
-    //         var newItemIndex = emotionGifs.indexOf(newItem);
-    //         if (newItemIndex > -1) {
-    //             emotionGifs.splice(newItemIndex, 1);
-    //         }
-    //         console.log(emotionGifs);
-    //         buttonPrint();
-    //     } else {
-    //         alert("You don't have a button for that!");
-    //     }
-
-    // });
-    
+    });  
 
     //when emotion button is pressed
 
     $("#emotionButtons").on("click", "button", function(){
-
+        
         $("#gifPanel").empty();
 
         var searchTerm = $(this).attr("data-value");
@@ -81,12 +58,7 @@ $(document).ready(function () {
         var apiURLBase = "https://api.giphy.com/v1/gifs/search?api_key=iiHxX5ZyrJst3sHz4lwHPzpO14Eot1nx&q=" + searchTerm + "&limit=10&offset=0&&lang=en";
 
 
-        // AJAX Call
-        console.log(this);
-        
-        console.log(searchTerm);
-        console.log(apiURLBase);
-        
+        // AJAX Call       
 
         $.ajax({
             url:apiURLBase,
@@ -94,13 +66,6 @@ $(document).ready(function () {
         }) .then(function(response){
 
             apiResponse = response;
-
-            console.log(apiURLBase);
-            console.log(response);
-            console.log(apiResponse);
-            
-
-
 
             for (var i = 0; i<apiResponse.data.length; i++) {
                 
@@ -119,16 +84,11 @@ $(document).ready(function () {
                 $(imageDiv).append("<p> GIF Parental Rating: " + apiResponse.data[i].rating + "</p>")
                 $(imageDiv).append(gifImage);
                 $("#gifPanel").append(imageDiv);
-
-
             }
-
-       
 
         });
 
     });
-
 
  $("body").on("click", "img", function(){
 
@@ -143,13 +103,8 @@ $(document).ready(function () {
                     $(this).attr("src", apiResponse.data[imageNumber].images.fixed_width_still.url);
                     $(this).attr("data-image_type", "still");
                 } 
-                
-                console.log(this);
-                console.log(imageFlag);
-                console.log($(this).attr("src"));
                  
             });
-  
 
 });
 
